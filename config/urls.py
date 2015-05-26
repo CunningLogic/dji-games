022eb716e5cc76007b58d80b23a94b1a3aea0f41
zlorenz <zglorenz@gmail.com>
2015-05-26 22:43:24 +0800

    Fixes ScrollTo id link conflict. Tests Juicer social feed page.

--- config/urls.py
+++ config/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name="home"),
     url(r'^agenda/$', TemplateView.as_view(template_name='pages/agenda.html'), name="agenda"),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name="about"),
+    url(r'^social/$', TemplateView.as_view(template_name='pages/social.html'), name="social"),
 
     # Django Admin
     url(r'^admin/', include(admin.site.urls)),
